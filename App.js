15a4459b54ebda9af5424bce90f143ceb4bcebcf
Abrar Hayat <abrarhayat@gmail.com>
2021-02-25 01:59:27 +0600

    Added Modal for input

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, TextInput, FlatList } from 'react-native';
+import screenStyle from './screenStyle';
+import { View, Button, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [allGoals, setAllGoals] = useState([]);
+  const [addMode, setAddMode] = useState(false);
   const addGoalHandler = (goalTitle) => {
     setAllGoals((prevGoals) => [...prevGoals, { id: Math.random().toString(), value: goalTitle }]);
+    setAddMode(false); //as we are setting two different states here, all states are updated and rerending happens only once
   }
   const deleteGoalHandler = (goalId) => {
     setAllGoals((prevGoals) => {
@@ -14,18 +17,12 @@ export default function App() {
     });
   }
   return (
-    <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} />
+    <View style={screenStyle}>
+      <Button title="Add New Goal" onPress={() => setAddMode(true)} />
+      <GoalInput modalVisible={addMode} setModalVisibility={setAddMode} onAddGoal={addGoalHandler} />
       <FlatList keyExtractor={(item, index) => item.id} data={allGoals} renderItem={(itemData) =>
         <GoalItem id={itemData.item.id} title={itemData.item.value} onDelete={deleteGoalHandler} />
       } />
     </View>
   );
 }
-const styles = StyleSheet.create({
-  screen: {
-    padding: 60,
-    flex: 1,
-    backgroundColor: 'rgba(0, 10, 2, 0.7)'
-  }
-});
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,32 +1,54 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Button } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Modal } from 'react-native';
+import screenStyle from '../screenStyle';
 
 const GoalInput = props => {
     const [enteredGoal, setEnteredGoal] = useState('');
     const goalInputHandler = (goalText) => {
         setEnteredGoal(goalText);
     }
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal);
+        setEnteredGoal('');
+    }
     return (
-        <View style={styles.inputContainer}>
-            <TextInput placeholder="Goals" placeholderTextColor="#8c9f8c" style={styles.input} onChangeText={goalInputHandler} />
-            {/* .bind() basically calls onAddGoal with the argument enteredGoal which is eventually passed on to App.js to addGoalHandler */}
-            <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
-        </View>
+        <Modal visible={props.modalVisible} animationType="slide">
+            <View style={[styles.inputContainer, screenStyle]}>
+                <TextInput placeholder="Goals" placeholderTextColor="#8c9f8c" style={styles.input} onChangeText={goalInputHandler} />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title="ADD" onPress={addGoalHandler} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="CANCEL" color="red" onPress={props.setModalVisibility.bind(this, false)} />
+                    </View>
+                </View>
+            </View>
+        </Modal>
     );
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: "row",
-        justifyContent: 'space-between',
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
     },
     input: {
-        width: '80%',
+        width: '95%',
         borderColor: 'black',
         borderWidth: 1,
         padding: 10,
+        margin: 20,
         color: 'greenyellow'
+    },
+    buttonContainer: {
+        width: "70%",
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    button: {
+        width: '40%'
     }
 });
 
